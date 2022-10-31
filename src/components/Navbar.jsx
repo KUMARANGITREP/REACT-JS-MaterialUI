5540956824f356900abcea4b16e34ff8460c07c0
KUMARAN <kumaran16102002@gmail.com>
2022-10-31 22:58:10 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import { Mail, Notifications, Pets } from '@mui/icons-material';
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import React, { useState } from 'react';
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  gap: '20px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: '10px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}));
+
+export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }}>
+          KUMARAN
+        </Typography>
+        <Pets sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={1} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={1} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://www.pexels.com/photo/gray-road-between-brown-mountains-11506984/"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://www.pexels.com/photo/gray-road-between-brown-mountains-11506984/"
+          />
+          <Typography variant="span">john</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClick={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
